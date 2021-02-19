176503fe127d549a9c884f3b6cc0eb2979736d76
erik <erik@librarystack.org>
2021-02-19 17:56:16 -0500

    prepare for ble connection with gnd-ctrl app

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
-
+#include <BLE2902.h>
 
 // https://btprodspecificationrefs.blob.core.windows.net/assigned-values/16-bit%20UUID%20Numbers%20Document.pdf
 /**
@@ -20,7 +20,7 @@
  * GATT Characteristic and Object Type 0x2A1D Temperature Type
  */
 #define SERVICE_UUID "8fc1ceca-b162-4401-9607-c8ac21383e4e"
-#define PRESSURE_CHARACTERISTIC_ID "2A6D" // standard characteristic for pressure in bars
+#define PRESSURE_CHARACTERISTIC_ID "c14f18ef-4797-439e-a54f-498ba680291d" // standard characteristic for pressure in bars
 #define BARS_UNIT_ID "2780"
 
 // Pins (set for ESP32 Devkit-c; see: https://circuits4you.com/2018/12/31/esp32-wroom32-devkit-analog-read-example/)
@@ -40,8 +40,8 @@ const int pressureSensorPin = 33;
 */
 volatile int rawPressure = 0;
 volatile float barPressure = 0;
-const int minRawPressure = 100;  // 1bar
-const int maxRawPressure = 4095; // ~10bar
+const int minRawPressure = 0;  // 1bar
+const int maxRawPressure = 3300; // ~10bar
 int rawPressureRange = maxRawPressure - minRawPressure;
 
 volatile int pumpLevel = 0;
@@ -53,9 +53,8 @@ volatile int encoderInterruptCount = 0;
 const int encoderPulsesPerRev = 40;
 const int encoderFullRangeRevs = 1;
 
-
-
 bool deviceConnected = false;
+bool oldDeviceConnected = false;
 
 portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;
 
@@ -64,6 +63,8 @@ ESP32Encoder encoder;
 //dimmerLamp pumpControl(pumpControlPin, pumpZeroCrossPin); //initialase port for dimmer for ESP8266, ESP32, Arduino due boards
 DimmableLight light(pumpControlPin);
 
+BLEServer *pServer = NULL;
+BLECharacteristic *pPressureCharacteristic = NULL;
 
 // https://github.com/nkolban/ESP32_BLE_Arduino/blob/master/examples/BLE_notify/BLE_notify.ino
 class ComGndServerCallbacks : public BLEServerCallbacks
@@ -105,43 +106,65 @@ void setup()
   // put your setup code here, to run once:
   //pumpControl.begin(NORMAL_MODE, ON); //dimmer initialisation: name.begin(MODE, STATE)
   Serial.begin(115200);
-  Serial.println("Ready to begin");
+  Serial.println("Setup start");
 
   DimmableLight::setSyncPin(pumpZeroCrossPin);
   DimmableLight::begin();
 
+  Serial.println("Pump Control Configured");
+
   encoder.attachHalfQuad(encoderPin1, encoderPin2);
   encoder.clearCount();
 
+  Serial.println("Encoder Configured");
+
   BLEDevice::init("COM-GND Espresso");
-  BLEServer *pServer = BLEDevice::createServer();
-  pServer->setCallbacks(new ComGndServerCallbacks());
+  Serial.println("BLE Device Initialized");
+  pServer = BLEDevice::createServer();
+  Serial.println("BLE Server Initialized");
   BLEService *pService = pServer->createService(SERVICE_UUID);
+  Serial.println("BLE Service Initialized");
+  pServer->setCallbacks(new ComGndServerCallbacks());
+    Serial.println("BLE Server Callback Initialized");
+
 
   // https://www.arduino.cc/en/Reference/ArduinoBLEBLECharacteristicBLECharacteristic
-  BLEFloatCharacteristic *pPressureCharacteristic = pService->createCharacteristic(
+  pPressureCharacteristic = pService->createCharacteristic(
       PRESSURE_CHARACTERISTIC_ID,
       BLECharacteristic::PROPERTY_READ |
           BLECharacteristic::PROPERTY_WRITE |
           BLECharacteristic::PROPERTY_NOTIFY |
           BLECharacteristic::PROPERTY_INDICATE);
 
+  Serial.println("BLE Pressure Characteristic Created");
+
   pPressureCharacteristic->setCallbacks(new PressureCallbacks());
-  pPressureCharacteristic->setValue("Hell World");
+
+  Serial.println("BLE Pressure Characteristic Callback Initialized");
+
+  pPressureCharacteristic->addDescriptor(new BLE2902());
+
   pService->start();
+
+  Serial.println("BLE Service Started");
+
   // BLEAdvertising *pAdvertising = pServer->getAdvertising();  // this still is working for backward compatibility
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  Serial.println("BLE BLEAdvertising Initialized");
+
   pAdvertising->addServiceUUID(SERVICE_UUID);
   pAdvertising->setScanResponse(true);
   pAdvertising->setMinPreferred(0x06); // functions that help with iPhone connections issue
   pAdvertising->setMinPreferred(0x12);
+  Serial.println("BLE BLEAdvertising Setup Complete");
+
   BLEDevice::startAdvertising();
-  Serial.println("Characteristic defined! Now you can read it in your phone!");
+   Serial.println("BLE Advertizing Started");
+  Serial.println("BLE Setup Complete");
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
   int oldCount = encoderInterruptCount;
   encoderInterruptCount = encoder.getCount();
 
@@ -162,7 +185,6 @@ void loop()
     float encoderPerc = encoderInterruptCount / (float)(encoderFullRangeRevs * encoderPulsesPerRev);
     pumpLevel = (int)((encoderPerc * (float)pumpRange) + pumpMin);
     //Serial.println(String(encoderInterruptCount) + " - " + String(pumpLevel));
-
   }
 
   if (lastPumpLevel != pumpLevel)
@@ -174,9 +196,39 @@ void loop()
     // TriacDimmer::setBrightness(channel_1, percVal);
     // delay(500);
   }
+ 
+  int lastRawPressure = rawPressure;
+  int rawPressure = analogRead(pressureSensorPin);
+  if (lastRawPressure != rawPressure)
+  {
+    int normalizeRawPressure = rawPressure - minRawPressure;
+    float rawPressurePerc = (float)((float)normalizeRawPressure / (float)rawPressureRange);
+    float barPressure = (rawPressurePerc * 10.0);
+    Serial.println(String(barPressure) + " - " + String(rawPressure) );
+    delay(250);
+    if (deviceConnected)
+    {
+      pPressureCharacteristic->setValue(barPressure);
+      pPressureCharacteristic->notify();
+      delay(3);
+    }
+  }
 
-  //int rawPressure = analogRead(pressureSensorPin);
-  //int voltage_value = (rawPressure * 3.3 ) / (4095);
+  // disconnecting
+  if (!deviceConnected && oldDeviceConnected)
+  {
+    delay(500); // give the bluetooth stack the chance to get things ready
+    pServer->startAdvertising(); // restart advertising
+    Serial.println("start advertising");
+    oldDeviceConnected = deviceConnected;
+  }
+  // connecting
+  if (deviceConnected && !oldDeviceConnected)
+  {
+    // do stuff here on connecting
+    oldDeviceConnected = deviceConnected;
+  }
+
   //Serial.println("Pressure: " + String(rawPressure));
   //delay(250);
   // float oldPressure = pressure;
